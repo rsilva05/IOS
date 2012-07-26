0655acf5c788ba41c37f845feedae88e65566171
Ricardo Silva <rsilva@aurum.com.br>
2012-07-26 18:33:34 +0100

    linha 01

--- teste04/ViewController.m
+++ teste04/ViewController.m
@@ -24,6 +24,7 @@
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    // linha 01
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
